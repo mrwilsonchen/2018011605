733730ddd8a5d8d8798ac28e1d4933db1f2bd190
C1 <c1@gjun.com>
2018-01-16 14:57:17 +0800

    2

--- app/src/main/java/com/wilson/a2018011605/MainActivity.java
+++ app/src/main/java/com/wilson/a2018011605/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -25,91 +27,9 @@ public class MainActivity extends AppCompatActivity {
     }
     public void click1(View v)
     {
-        String str = getFilesDir().getAbsolutePath();
-        Log.d("FILE", str);
-        String str1 = getCacheDir().getAbsolutePath();
-        Log.d("FILE", str1);
-    }
-    public void click2(View v)
-    {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 
-        File f = new File(getFilesDir(), "myfile.txt");
-        try {
-            FileWriter fw = new FileWriter(f);
-            fw.write("Hello World");
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void click3(View v)
-    {
-        ArrayList<String> mylist = new ArrayList();
-        mylist.add("Bob");
-        mylist.add("Mary");
-        mylist.add("Peter");
-        File f = new File(getFilesDir(), "myfile1.txt");
-        try {
-            FileWriter fw = new FileWriter(f);
-            Gson gson = new Gson();
-            String data = gson.toJson(mylist);
-            fw.write(data);
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void click4(View v)
-    {
-        ArrayList<Student> mydata = new ArrayList();
-        mydata.add(new Student(1, "Bob", 95));
-        mydata.add(new Student(2, "Mary", 98));
-        mydata.add(new Student(3, "Peter", 70));
-
-        File f = new File(getFilesDir(), "myfile2.txt");
-        try {
-            FileWriter fw = new FileWriter(f);
-            Gson gson = new Gson();
-            String data = gson.toJson(mydata);
-            fw.write(data);
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void clickRead1(View v)
-    {
-        File f = new File(getFilesDir(), "myfile1.txt");
-        try {
-            FileReader fr = new FileReader(f);
-            BufferedReader br = new BufferedReader(fr);
-            String str = br.readLine();
-            Log.d("FILE", str);
-            Gson gson = new Gson();
-            ArrayList<String> mydata = gson.fromJson(str, new TypeToken<ArrayList<String>>(){}.getType());
-            for (String s :  mydata)
-            {
-                Log.d("FILE", "data:" + s);
-            }
-            br.close();
-            fr.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-    class Student
-    {
-        public int id;
-        public String name;
-        public int score;
-        public Student(int id, String name, int score)
-        {
-            this.id = id;
-            this.name = name;
-            this.score = score;
-        }
+        myRef.setValue("Hello, World!");
     }
 }
